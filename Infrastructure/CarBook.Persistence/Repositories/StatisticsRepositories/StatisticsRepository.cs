a42e15dcf7290a16bf6453acbd85d75715774b1b
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-01-15 14:16:17 +0300

    update_15.01.2025

---
+++ Infrastructure/CarBook.Persistence/Repositories/StatisticsRepositories/StatisticsRepository.cs
@@ -0,0 +1,114 @@
+﻿using CarBook.Application.Interfaces.StatisticsInterfaces;
+using CarBook.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Repositories.StatisticsRepositories
+{
+    public class StatisticsRepository : IStatisticsRepository
+    {
+        private readonly CarBookContext _context;
+        public StatisticsRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public int GetAuthorCount()
+        {
+            var value = _context.Authors.Count();
+            return value;
+        }
+
+        public decimal GetAvgRentPriceForDailey()
+        {
+            int id = _context.Pricings.Where(x=> x.Name == "Günlük").Select(x=> x.PricingID).FirstOrDefault();
+            var value = _context.CarPricings.Where(x=> x.PricingID == id).Average(x=> x.Amount);
+            return value;
+        }
+
+        public decimal GetAvgRentPriceForMonthly()
+        {
+            int id = _context.Pricings.Where(x => x.Name == "Aylık").Select(x => x.PricingID).FirstOrDefault();
+            var value = _context.CarPricings.Where(x => x.PricingID == id).Average(x => x.Amount);
+            return value;
+        }
+
+        public decimal GetAvgRentPriceForWeekly()
+        {
+            int id = _context.Pricings.Where(x => x.Name == "Haftalık").Select(x => x.PricingID).FirstOrDefault();
+            var value = _context.CarPricings.Where(x => x.PricingID == id).Average(x => x.Amount);
+            return value;
+        }
+
+        public int GetBlogCount()
+        {
+            var value = _context.Blogs.Count();
+            return value;
+        }
+
+        public string GetBlogTitleByMaxBlogComment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBrandCount()
+        {
+            var value = _context.Brands.Count();
+            return value;
+        }
+
+        public string GetBrandNameByMaxCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCarBrandAndModelByRentPriceDaileyMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCarBrandAndModelByRentPriceDaileyMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCarCount()
+        {
+            var value = _context.Cars.Count();
+            return value;
+        }
+
+        public int GetCarCountByFualElectiric()
+        {
+            var value = _context.Cars.Where(x => x.Fuel == "Elektirikli").Count();
+            return value;
+        }
+
+        public int GetCarCountByFualGasolineOrDiesel()
+        {
+            var value = _context.Cars.Where(x=> x.Fuel == "Benzin" || x.Fuel == "Dizel").Count();
+            return value;
+        }
+
+        public int GetCarCountByKmSmallerThan1000()
+        {
+            var value = _context.Cars.Where(x=> x.Km <= 1000).Count();
+            return value;
+        }
+
+        public int GetCarCountByTranmissionAuto()
+        {
+            var value = _context.Cars.Where(x => x.Transmission == "Otomatik").Count();
+            return value;
+        }
+
+        public int GetLocationCount()
+        {
+            var value = _context.Locations.Count();
+            return value;
+        }
+    }
+}
--- Presentation/CarBook.WebApi/Controllers/CarsController.cs
+++ Presentation/CarBook.WebApi/Controllers/CarsController.cs
@@ -1,8 +1,6 @@
 ﻿using CarBook.Application.Features.CQRS.Commands.CarCommands;
 using CarBook.Application.Features.CQRS.Handlers.CarHandlers;
 using CarBook.Application.Features.CQRS.Queries.CarQueries;
-using CarBook.Application.Features.CQRS.Results.CarResults;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarBook.WebApi.Controllers
@@ -81,6 +79,6 @@ namespace CarBook.WebApi.Controllers
         {
             var values = _getLast5CarsWithBrandQueryHandler.Handle();
             return Ok(values);
-        }       
+        }
     }
 }
